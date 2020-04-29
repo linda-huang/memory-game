d4f8cc3046791d093f410655e7d371852b6ed3ee
Linda Huang <sh837@cornell.edu>
2020-04-29 17:14:17 +0800

    added user prompts and image clickability

--- memory-game-app/src/components/card/allCards.js
+++ memory-game-app/src/components/card/allCards.js
@@ -1,26 +1,35 @@
 import React, { useState } from 'react'; 
 import MemoryCard from './memoryCard';
+import UserPrompt from './userPrompts'
 import mountain from '../images/mountain.jpg';
 import tree from '../images/tree.jpg';
 import pavilion from '../images/pavilion.jpg';
 import lanterns from '../images/lanterns.jpg';
+import cornell from '../images/cornell.jpg';
 import '../../App.css';
 
 export default function AllCards () {
 
     const [imgList, setImgList] = useState([mountain, tree, pavilion, lanterns]);
+    const [numFlip, setNumFlip] = useState(0);
+    const [flippedImgs, setFlippedImgs] = useState([]);
 
     const generateAllCards = () => {
         return(
-            imgList.map((img, index) =>
-            <MemoryCard imgURL = {img} alt={index}/>
+            imgList.map((img, index) =>(
+                <MemoryCard numFlip = {numFlip} updateFlippedImgs = {(input) => setFlippedImgs(input)}
+                    updateNumFlip = {(input) => setNumFlip(input)} imgURL = {img} alt={index} defaultPic={cornell}/>
+                )
             )
         )
     }
 
     return (
-        <div className='cardContainer'>
-            {generateAllCards()}
+        <div>
+            <div className='cardContainer'>
+                {generateAllCards()}
+            </div>
+            <UserPrompt className='userPrompt' numFlip = {numFlip}/>
         </div>
     )
 } 
