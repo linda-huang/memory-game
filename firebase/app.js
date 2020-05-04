e418bdeb5014b4846a35768bae4209b289db596a
Yang <ys656@cornell.edu>
2020-05-04 17:28:10 -0400

    updated dbimageList and tried to implement randomization

--- firebase/app.js
+++ firebase/app.js
@@ -2,6 +2,7 @@ const admin = require('firebase-admin');
 const serviceAccount = require('./service-account.json');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -14,27 +15,30 @@ const app = express();
 app.use(bodyParser.json());
 const port = 8080;
 
-app.get('/', function(req, res){
+app.get('/', cors(), function(req, res){
     res.send('Welcome home!')
 });
 
 const imageCollection = db.collection('images');
 
-app.post('/post', function(req, res){
+app.post('/post', cors(), function(req, res){
     const image = req.body;
     imageCollection.doc().set(image);
     res.send('image created!');
 });
 
-app.get('/post', async function(req, res){
-    const allImagesDoc = await imageCollection.get();
-    const images = [];
-    for(let doc of allImagesDoc.docs){
-        let image = doc.data();
-        image.id = doc.id;
-        images.push(image);
+app.get('/post', cors(), async function(req, res){
+    try{
+        const allImagesDoc = await imageCollection.get();
+        const images = [];
+        for(let doc of allImagesDoc.docs){
+            let image = doc.data();
+            image.id = doc.id;
+            images.push(image);
+        }
+        res.send(images);
     }
-    res.send(images);
+    catch(error){console.log("error")};
 });
 
 app.listen(port, function() {console.log('app started')});
--- memory-game-app/src/components/card/allCards.js
+++ memory-game-app/src/components/card/allCards.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import MemoryCard from './memoryCard';
 import UserPrompt from './userPrompts'
 import mountain from '../images/mountain.jpg';
@@ -7,14 +7,60 @@ import pavilion from '../images/pavilion.jpg';
 import lanterns from '../images/lanterns.jpg';
 import cornell from '../images/cornell.jpg';
 import '../../App.css';
+// import { response } from 'express';
 
 export default function AllCards () {
 
-    const [imgList, setImgList] = useState([mountain, tree, pavilion, lanterns]);
+    const numImg = 4;
+
+    const [imgList, setImgList] = useState([]);
+    // const [imgList, setImgList] = useState([mountain, tree, pavilion, lanterns]);
     const [numFlip, setNumFlip] = useState(0);
     const [flippedImgs, setFlippedImgs] = useState([]);
 
+    const [dbimgList, setdbImgList] = useState([
+        {
+            "name" : '',
+            "url" : '',
+            "id" : ''
+        }
+    ]);
+    
+    useEffect(() => {
+        fetch('http://localhost:8080/post')
+            .then(res => res.json())
+            .then(data => setdbImgList(data));
+    });
+
+    
+    // console.log(dbimgList);
+    // console.log(dbimgList[0].url);
+
+    function generateImgList(num){
+        var i;
+        for (i = 0; i < num; i++){
+            const len = dbimgList.length;
+            // console.log(dbimgList.length);
+            const ram = Math.floor(Math.random() * len);
+            console.log(ram);
+            // console.log(dbimgList);
+            // imgList[i] = (dbimgList[ram].url);
+        }
+    }
+
+    // useEffect((num) => {
+    //     var i;
+    //     for (i = 0; i < num; i++){
+    //         const ram = Math.floor(Math.random() * dbimgList.length);
+    //         console.log(ram);
+    //         setImgList(dbimgList[ram].url);
+    //     }
+    // })
+
+
     const generateAllCards = () => {
+        generateImgList(numImg);
+        // console.log(imgList);
         return(
             imgList.map((img, index) =>(
                 <MemoryCard numFlip = {numFlip} updateFlippedImgs = {(input) => setFlippedImgs(input)}
@@ -27,6 +73,7 @@ export default function AllCards () {
     return (
         <div>
             <div className='cardContainer'>
+                {/* {getCardStack()} */}
                 {generateAllCards()}
             </div>
             <UserPrompt className='userPrompt' numFlip = {numFlip}/>
