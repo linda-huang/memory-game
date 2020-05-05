4566a55b58403ea1233319ceaaeba82a72b51304
Yang <ys656@cornell.edu>
2020-05-05 16:50:52 -0400

    tried with solving the infinite fetching problem

--- memory-game-app/src/components/card/allCards.js
+++ memory-game-app/src/components/card/allCards.js
@@ -7,17 +7,14 @@ import pavilion from '../images/pavilion.jpg';
 import lanterns from '../images/lanterns.jpg';
 import cornell from '../images/cornell.jpg';
 import '../../App.css';
-// import { response } from 'express';
 
 export default function AllCards () {
 
     const numImg = 4;
-
     const [imgList, setImgList] = useState([]);
     // const [imgList, setImgList] = useState([mountain, tree, pavilion, lanterns]);
     const [numFlip, setNumFlip] = useState(0);
     const [flippedImgs, setFlippedImgs] = useState([]);
-
     const [dbimgList, setdbImgList] = useState([
         {
             "name" : '',
@@ -26,24 +23,19 @@ export default function AllCards () {
         }
     ]);
     
-    useEffect(() => {
+    const fetchDBImage = () => {
         fetch('http://localhost:8080/post')
-            .then(res => res.json())
-            .then(data => setdbImgList(data));
-    });
-
-    
-    // console.log(dbimgList);
-    // console.log(dbimgList[0].url);
+                .then(res => res.json())
+                .then(data => setdbImgList(data));
+    }
 
     function generateImgList(num){
+            fetchDBImage();
+        console.log(dbimgList);
         var i;
         for (i = 0; i < num; i++){
             const len = dbimgList.length;
-            // console.log(dbimgList.length);
             const ram = Math.floor(Math.random() * len);
-            console.log(ram);
-            // console.log(dbimgList);
             // imgList[i] = (dbimgList[ram].url);
         }
     }
@@ -52,7 +44,6 @@ export default function AllCards () {
     //     var i;
     //     for (i = 0; i < num; i++){
     //         const ram = Math.floor(Math.random() * dbimgList.length);
-    //         console.log(ram);
     //         setImgList(dbimgList[ram].url);
     //     }
     // })
@@ -60,7 +51,6 @@ export default function AllCards () {
 
     const generateAllCards = () => {
         generateImgList(numImg);
-        // console.log(imgList);
         return(
             imgList.map((img, index) =>(
                 <MemoryCard numFlip = {numFlip} updateFlippedImgs = {(input) => setFlippedImgs(input)}
@@ -73,7 +63,6 @@ export default function AllCards () {
     return (
         <div>
             <div className='cardContainer'>
-                {/* {getCardStack()} */}
                 {generateAllCards()}
             </div>
             <UserPrompt className='userPrompt' numFlip = {numFlip}/>
