4a59975c67c706b647695952d79ab22dafacd920
Linda Huang <sh837@cornell.edu>
2020-05-12 10:30:15 +0800

    fixed randomization bug

--- memory-game-app/src/components/card/allCards.js
+++ memory-game-app/src/components/card/allCards.js
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from 'react'; 
 import MemoryCard from './memoryCard';
 import UserPrompt from './userPrompts'
-import mountain from '../images/mountain.jpg';
-import tree from '../images/tree.jpg';
-import pavilion from '../images/pavilion.jpg';
-import lanterns from '../images/lanterns.jpg';
 import cornell from '../images/cornell.jpg';
 import victory from '../images/victory_bear.jpg'
 import '../../App.css';
@@ -16,7 +12,6 @@ export default function AllCards () {
     const [init, setInit] = useState(1);
     const [dataAcquired, setDataAcquired] = useState(0);
     const timeout = setTimeout(() => setInit(0), 3000);
-    const interval = setInterval(randomization, 300);
 
     const [victoryIndex, setVictoryIndex] = useState([]);
     const [totalVictory, setTotalVictory] = useState(false);
@@ -32,22 +27,24 @@ export default function AllCards () {
     useEffect(() => {
         fetch('http://localhost:8080/post')
                 .then(res => res.json())
-                .then(data => setdbImgList(data));
-        setDataAcquired(1);
+                .then(data => {
+                    randomization(data);
+                });
     }, [])
 
-    function randomization(){
-        if(dataAcquired == 1){
+    function randomization(data){
+            var tempImgList = [];
             var i;
-            for (i = 0; i < dbimgList.length; i++){
-                const ram = Math.floor(Math.random() * dbimgList.length);
-                var temp = dbimgList[i];
-                dbimgList[i] = dbimgList[ram];
-                dbimgList[ram] = temp;
+            for (i = 0; i < data.length; i++){
+                tempImgList[i] = data[i];
             }
-            setDataAcquired(0);
-            clearInterval(interval);
-        }
+            for (i = 0; i < data.length; i++){
+                const ram = Math.floor(Math.random() * data.length);
+                var temp = tempImgList[i];
+                tempImgList[i] = tempImgList[ram];
+                tempImgList[ram] = temp;
+            }
+            setdbImgList(tempImgList);
     }
 
     const generateAllCards = () => {
@@ -73,26 +70,10 @@ export default function AllCards () {
             )
         )
     }
-
-    // const generateAllCards = () => {
-    //     return(
-    //         imgList.guessingPic.map((img, index) => (
-    //             (victoryIndex.includes(index)) ?
-    //                     (<figure>
-    //                         <img src={imgList.winPic} alt={index} key={index} width="200" height="200"/>
-    //                     </figure>) :
-    //                     (<MemoryCard numFlip = {numFlip} 
-    //                         alt = {index} 
-    //                         flippedImgs = {flippedImgs}   
-    //                         updateFlippedImgs = {(input) => setFlippedImgs(input)}
-    //                         updateNumFlip = {(input) => setNumFlip(input)} 
-    //                         imgURL = {img} 
-    //                         otherURL={imgList}/>)
  
     const compareImgs = () => {
             if (numFlip === 2 && flippedImgs.length !== 0) {
                 if (flippedImgs[0].imgURL === flippedImgs[1].imgURL) {
-                    console.log("AMAZING PAIRED IMGS")
                     setVictoryIndex(victoryIndex.concat([flippedImgs[0].alt,flippedImgs[1].alt]))
                     setFlippedImgs([]);
                     setNumFlip(0);
